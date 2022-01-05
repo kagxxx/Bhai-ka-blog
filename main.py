c8b6985f0d271f56b06075dda3889c18af631e96
Kunal <kunnalgakhar1728@gmail.com>
2022-01-05 12:53:49 +0530

    initial commit

---
+++ main.py
@@ -0,0 +1,172 @@
+from flask import Flask, render_template, request, session, redirect
+from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
+from flask_mail import Mail
+from werkzeug import secure_filename
+import json
+import os
+import math
+
+with open('config.json', 'r') as c:
+    params = json.load(c)["params"]
+
+local_server = True
+app = Flask(__name__)
+app.secret_key = 'super-secret-key'
+app.config['UPLOAD_FOLDER'] = params['upload_location']
+app.config.update(
+    MAIL_SERVER="smtp.gmail.com",
+    MAIL_PORT="465",
+    MAIL_USE_SSL=True,
+    Mail_USERNAME=params['gmail-user'],
+    Mail_PASSWORD=params['gmail-password']
+)
+mail = Mail(app)
+if (local_server):
+    app.config['SQLALCHEMY_DATABASE_URI'] = params['local_uri']
+else:
+    app.config['SQLALCHEMY_DATABASE_URI'] = params['prod_uri']
+db = SQLAlchemy(app)
+
+
+class Contacts(db.Model):
+    '''
+    serial number, name, email, phone_number, message, date
+    '''
+    serial_number = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(80), nullable=False)
+    email = db.Column(db.String(20), unique=True, nullable=False)
+    phone_number = db.Column(db.String(12), unique=True, nullable=False)
+    message = db.Column(db.String(120), nullable=False)
+    date = db.Column(db.String(12))
+
+
+class Posts(db.Model):
+    '''
+    serial number, name, email, phone_number, message, date
+    '''
+    sno = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(80), nullable=False)
+    slug = db.Column(db.String(21), unique=True, nullable=False)
+    content = db.Column(db.String(120), nullable=False)
+    tagline = db.Column(db.String(120), nullable=False)
+    date = db.Column(db.String(12))
+    img_file = db.Column(db.String(12), nullable=True)
+
+
+@app.route("/")
+def home():
+    posts = Posts.query.filter_by().all()
+    last = math.ceil(len(posts)/int(params['number_of_post']))
+    page = request.args.get('page')
+    if(not str(page).isnumeric()):
+        page = 1
+    page = int(page)
+    posts = posts[(page-1)*int(params['number_of_post']):(page-1)*int(params['number_of_post'])+int(params['number_of_post'])]
+    if page == 1:
+        prev = "#"
+        next = "/?page="+str(page+1)
+    elif page == last:
+        next = "#"
+        prev = "/?page=" + str(page - 1)
+    else:
+        prev = "/?page="+str(page-1)
+        next = "/?page="+str(page+1)
+
+    return render_template("index.html", params=params, posts=posts, prev=prev, next=next)
+
+
+@app.route("/post/<string:post_slug>", methods=['GET'])
+def post_route(post_slug):
+    post = Posts.query.filter_by(slug=post_slug).first()
+
+    return render_template('post.html', params=params, post=post)
+
+
+@app.route("/about")
+def about():
+    return render_template("about.html", params=params)
+
+
+@app.route("/dashboard", methods=['GET', 'POST'])
+def dashboard():
+    if 'user' in session and session['user'] == params['admin_user']:
+        posts = Posts.query.all()
+        return render_template("dashboard.html", params=params, posts=posts)
+    if request.method == 'POST':
+        username = request.form.get('uname')
+        userpass = request.form.get('pass')
+        if username == params['admin_user'] and userpass == params['admin_password']:
+            # set the session variable
+            session['user'] = username
+            posts = Posts.query.all()
+            return render_template("dashboard.html", params=params, posts=posts)
+    posts = Posts.query.all()
+    return render_template('dashboard.html', params=params, posts=posts)
+
+
+@app.route("/edit/<string:sno>", methods=["GET", "POST"])
+def edit(sno):
+    if request.method == 'POST':
+        box_title = request.form.get('Title')
+        tagline = request.form.get('Tagline')
+        slug = request.form.get('Slug')
+        content = request.form.get('Content')
+        img_file = request.form.get('img_file')
+        date = datetime.now()
+
+        if sno == "0":
+            post = Posts(title=box_title, slug=slug, content=content, tagline=tagline, img_file=img_file, date=date)
+            db.session.add(post)
+            db.session.commit()
+        else:
+            post=Posts.query.filter_by(sno=sno).first()
+            post.title=box_title
+            post.slug=slug
+            post.content=content
+            post.tagline=tagline
+            post.img_file=img_file
+            post.date=date
+            db.session.commit()
+            return redirect('/edit/'+sno)
+    post = Posts.query.filter_by(sno=sno).first()
+    return render_template('edit.html', params=params, post=post, sno=sno)
+
+@app.route("/uploader", methods=["GET", "POST"])
+def uploader():
+    if (request.method=='POST'):
+        f = request.files['file1']
+        f.save(os.path.join(app.config['UPLOAD_FOLDER'], secure_filename(f.filename)))
+        return "Uploaded successfully"
+
+@app.route("/logout")
+def logout():
+    return redirect('/dashboard')
+
+@app.route("/delete/<string:sno>", methods=['GET', 'POST'])
+def delete(sno):
+    post = Posts.query.filter_by(sno=sno).first()
+    db.session.delete(post)
+    db.session.commit()
+    return redirect('/dashboard')
+
+
+@app.route("/contact", methods=["GET", "POST"])
+def contact():
+    if request.method == "POST":
+        ''' add entry to the database'''
+        name = request.form.get('name')
+        email = request.form.get('email')
+        phone = request.form.get('phone')
+        message = request.form.get('message')
+
+        entry = Contacts(name=name, phone_number=phone, message=message, date=datetime.now(), email=email)
+        db.session.add(entry)
+        db.session.commit()
+        mail.send_message('New Message from' + name, sender='email', recipients=[params['gmail-user']],
+                          body=message + "\n" + phone)
+
+    return render_template("contact.html", params=params)
+
+
+app.run(debug=True)
